d564db28f5462acb2493a59c80b06c054d4c58b3
yeongje Seo <yeongjeseo@yeongjeui-MacBookPro.local>
2021-05-24 15:48:09 +0900

    fullStackExcemtion & emptyStackExcetion 생성

--- shared/constants.js
+++ shared/constants.js
@@ -1,7 +1,24 @@
 class NoSuchElementException extends Error {
     constructor(){
+        super();
         this.message = 'No Such Element.';
     }
 }
 
+class FullStackExcetion extends Error {
+    constructor(){
+        super();
+        this.message = 'Full Stack.';
+    }
+}
+
+class EmptyStackException extends Error {
+    constructor(){
+        super();
+        this.message = 'Empty Stack.';
+    }
+}
+
 module.NoSuchElementException = NoSuchElementException;
+module.FullStackExcetion = FullStackExcetion;
+module.EmptyStackException = EmptyStackException;
